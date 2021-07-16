394047a2661ac63e969e1eb617e61f7e590d7156
Atlas <jack@bentsen.us>
2021-07-16 12:37:13 -0700

    initial commit with employee constructor

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,17 @@
+// Write code to define and export the Employee class
+function Employee(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.getName = () => {
+        return this.name
+    };
+    this.getEmail = () => {
+        return this.email
+    };
+    this.getRole = () => {
+        return 'Employee'
+    }
+};
+
+module.exports = Employee
