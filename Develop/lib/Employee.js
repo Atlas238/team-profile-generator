84433256b36dab2c6177a59a774c6794a3baa80a
Atlas <jack@bentsen.us>
2021-07-16 23:03:57 -0700

    adding role to base employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -4,18 +4,27 @@ class Employee {
         this.name = name;
         this.id = id;
         this.email = email;
+        this.role = employee;
     }
 
     getName = () => {
         console.log(`This Employee's name is ${this.name}`);
+        return this.name;
     }
 
     getEmail = () => {
         console.log(`${this.name}'s email is ${this.email}`);
+        return this.email;
     }
     
     getID = () => {
         console.log(`${this.name}'s id is ${this.id}`);
+        return this.id;
+    }
+
+    getRole = () => {
+        console.log(`${this.name}'s role is ${this.role}`);
+        return this.role;
     }
 }
 
