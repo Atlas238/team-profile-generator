c10a8f295a99aeecb7d3a242eb36e814a1341998
Atlas <jack@bentsen.us>
2021-07-17 14:52:57 -0700

    output works! now to style

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Employee = require('./lib/Employee.js');
 const Engineer = require('./lib/Engineer.js');
 const Manager = require('./lib/Manager.js');
 const Intern = require('./lib/Intern.js');
-
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
 
 const teamManagerQuestions = [
     {
@@ -115,7 +107,7 @@ const newIntern = async () => {
     console.log("Thanks! Your new Intern has been added.");
 }
 
-const generateHTML = async () => {
+const generateHTML = () => {
     
     let template = `<!DOCTYPE html>
     <html lang="en">
@@ -219,24 +211,23 @@ const generateHTML = async () => {
                 
             cardStack.push(`${card}\n`);
             } break;
+        } 
+    });
+
+    const fileText = addCardStack(template, 455, cardStack.join(""))
+    
+    fs.writeFile('./Output/index.html', fileText, (err) => {
+        if (err) console.log(err);
+        else {
+            console.log('All Done! Check the output folder to find your new index.html file!');
         }
-        // Should have each employees details saved to a big card string in an array
-        console.log(cardStack);
-        // Adds the stack string to the middle of our template page (main div section)
-        console.log(addCardStack(template, 497, cardStack));
-        
-        // TODO: confirm that final template gen works, and check on output
-        
     });
 }
 
 const main = async () => {
 
-    // WHEN I start the application
-    // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
     await registerManager();
     
-    // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
     do {
         const { another } = await inquirer.prompt({
             type:'list',
